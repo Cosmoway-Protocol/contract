39dbd9012e852a6f4cffd0af221f1aa223b34b33
flyinglimao <hi@limaois.me>
2023-01-27 08:37:42 +0800

    chore: deploy to hyperspace testnet

--- script/CosmowayProtocol.s.sol
+++ script/CosmowayProtocol.s.sol
@@ -2,11 +2,22 @@
 pragma solidity ^0.8.16;
 
 import "forge-std/Script.sol";
+import "../src/CosmowayProtocol.sol";
+import "../src/conditioners/AssetConditioner.sol";
 
-contract CounterScript is Script {
-    function setUp() public {}
-
+contract CosmowayProtocolScript is Script {
     function run() public {
-        vm.broadcast();
+        uint deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+        vm.startBroadcast(deployerPrivateKey);
+
+        // deploy main protocol
+        CosmowayProtocol cosmoway = new CosmowayProtocol();
+
+        // deploy asset conditioners
+        ERC20Conditioner erc20Conditioner = new ERC20Conditioner();
+        ERC721Conditioner erc721Conditioner = new ERC721Conditioner();
+        ERC1155Conditioner erc1155Conditioner = new ERC1155Conditioner();
+
+        vm.stopBroadcast();
     }
 }
